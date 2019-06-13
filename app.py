1971c8f32ba0cccd314f6890fb1218d56f9812a6
student <sorush.kh68@gmail.com>
2019-06-13 13:36:13 +0100

    We can show all stores and one store based on its name.

--- app.py
+++ app.py
@@ -1,20 +1,47 @@
-from flask import Flask
+from flask import Flask, jsonify
 import json
 
 app = Flask(__name__)
+
+stores = [
+{ 'name':'My favorite store',
+  'items':[
+    {'name':'The book', 'price':15.99}
+]
+}
+]
+
+
 
 # POST - used to receive data
 # GET - used to send data back only
 
 # POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
 
 # GET /store/<string:name>
+@app.route('/store/<string:name>', methods=['GET'])
+def show_store(name):
+    for store in stores:
+        if store['name'] == name :
+            return jsonify(store)
+    return("The store not found!")
 
 # GET /stores
+@app.route('/store')
+def get_stores():
+    return(jsonify({'stores':stores}))
 
 # POST store/<string:name >/item {name:,price:}
-
-# GET store/<string:name> /item/<string name:>
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item(name):
+    pass
+# GET store/<string:name> /item/<string:item_name>
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
 
 
 app.run(port=5000)
